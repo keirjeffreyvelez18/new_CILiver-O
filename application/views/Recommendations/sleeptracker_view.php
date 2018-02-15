f51de3c09ce0736b9a86155d87bc7caff5ea319e
keirjeffreyvelez <keirjeffreyvelez18@gmail.com>
2018-02-16 03:56:36 +0800

    create sleepPattern.php, waterTracker.php, display sleep pattern in graph, and fix minor error

--- application/views/Recommendations/sleeptracker_view.php
+++ application/views/Recommendations/sleeptracker_view.php
@@ -17,10 +17,12 @@
 							<div class="row">
 								<form method="POST" action="<?php echo base_url('index.php/sleeptracker/saveSleepTracker');?>">
 									<div class="col-md-12">
-										<label for="dos">Date Of Sleep</label>
+										<label for="dos" class="text-muted">Date Of Sleep</label>
 										<input id="dos" class="form-control" type="date" name="dateOfSleep" value="<?php echo date('Y-m-d'); ?>"></input><br>
-										<input class="form-control" name="sleeptime" type="time" placeholder="Sleep Time..." value="00:00"></input><br>
-										<input class="form-control" name="wakeuptime" type="time" placeholder="Wake Up Time..." value="00:00"></input><br>
+										<label for="st" class="text-muted">Sleep Time</label>
+										<input id="st" class="form-control" name="sleeptime" type="time" placeholder="Sleep Time..." value="00:00"></input><br>
+										<label for="wt" class="text-muted">Wake Up Time</label>
+										<input id="wt" class="form-control" name="wakeuptime" type="time" placeholder="Wake Up Time..." value="00:00"></input><br>
 										<input type="submit" name="" value="Save" class="form-control btn btn-success">
 									</div>
 								</form><br>
@@ -29,88 +31,7 @@
 						</div>
 						<!-- InputForm -->
 					  <!-- Sleep Progress Go Here -->
-					  <script type="text/javascript">
-					  	window.onload = function () {
-
-							var chart = new CanvasJS.Chart("chartContainer", {
-								animationEnabled: true,
-								theme: "light2",
-								title:{
-									text: "Sleeping Pattern"
-								},
-								axisX:{
-									valueFormatString: "DD MMM",
-									crosshair: {
-										enabled: true,
-										snapToDataPoint: true
-									},
-									ValueFormatString: "DD MMM, YYYY",
-								},
-								axisY: {
-									title: "Hours",
-									crosshair: {
-										enabled: true
-									},
-									ValueFormatString: "HH:mm",
-								},
-								toolTip:{
-									shared:true
-								},  
-								legend:{
-									cursor:"pointer",
-									verticalAlign: "bottom",
-									horizontalAlign: "left",
-									dockInsidePlotArea: true,
-									itemclick: toogleDataSeries
-								},
-								data: [{
-									type: "line",
-									showInLegend: true,
-									name: "Fall Asleep",
-									markerType: "square",
-									color: "#F08080",
-									dataPoints: [
-										{ x: new Date(2017, 0, 3), y: new Date(21, 0, 1) },
-										{ x: new Date(2017, 0, 4), y: 22 },
-										{ x: new Date(2017, 0, 5), y: 23 },
-										{ x: new Date(2017, 0, 6), y: 20 },
-										{ x: new Date(2017, 0, 7), y: 22 },
-										{ x: new Date(2017, 0, 8), y: 22 },
-
-									]
-								},
-								{
-									type: "line",
-									showInLegend: true,
-									name: "Wake Up",
-									lineDashType: "dash",
-									dataPoints: [
-										{ x: new Date(2017, 0, 3), y: new Date(21, 0, 1) },
-										{ x: new Date(2017, 0, 4), y: 03 },
-										{ x: new Date(2017, 0, 5), y: 05 },
-										{ x: new Date(2017, 0, 6), y: 05 },
-										{ x: new Date(2017, 0, 7), y: 07 },
-										{ x: new Date(2017, 0, 8), y: 04 },
-									]
-								}]
-							});
-							chart.render();
-
-						function toogleDataSeries(e){
-							if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-								e.dataSeries.visible = false;
-							} else{
-								e.dataSeries.visible = true;
-							}
-							chart.render();
-						}
-
-						}
-					  </script>
-					  <br>
-					  <div class = "col-lg-12">
-					  	<div id="chartContainer" style="height: 300px; width: 100%;"></div>
-					  </div>
+					  	<?php include_once('Lib/layout/sleepPattern.php');?>
 					  <!-- Sleep Progress Go Here -->
 
 					  	<hr>
--- application/views/Recommendations/watertracker_view.php
+++ application/views/Recommendations/watertracker_view.php
@@ -21,41 +21,8 @@
 							<input type="submit" name="" value="Save" style="color: black">
 						</form>
 					</div>
-					<script type="text/javascript">
-						window.onload = function () {
-
-							var chart = new CanvasJS.Chart("chartContainer", {
-								animationEnabled: true,  
-								exportEnabled: true,
-								title:{
-									text: "Water Intake"
-								},
-								axisY: {
-									title: "Number of Glasses",
-									suffix: " Glasses",
-									stripLines: [{
-										value: 8,
-										label: "Average",
-										valueFormatString: "DD MMMM YYYY"
-									}],
-
-								},
-								data: [{
-									yValueFormatString: "## Glasses",
-									type: "line",
-									dataPoints: [
-									<?php foreach ($waterintakeData as $row): ?>
-										<?php $d = explode('-', $row['dateOfIntake']); ?>
-										{ x: new Date(<?php echo $d[0].",".($d[1]-1).",".$d[2]; ?>), y: <?php echo $row['numberOfWaterIntake'] ?> },	
-									<?php endforeach ?>
-									]
-								}]
-							});
-							chart.render();
-
-							}
-					</script>
-					<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+					
+					<?php include_once('Lib/layout/waterTracker.php');?>
 
 				</center><br>
 				</div>
