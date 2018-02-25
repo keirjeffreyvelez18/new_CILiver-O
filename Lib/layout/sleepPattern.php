e1fe767f6cca6f2b27b397f143c90e20478625d7
keirjeffreyvelez <keirjeffreyvelez18@gmail.com>
2018-02-25 08:10:24 +0800

    add number of hrs slept by the patient in sleep tracker graph

--- Lib/layout/sleepPattern.php
+++ Lib/layout/sleepPattern.php
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+						<script type="text/javascript">
 					  		window.onload = function () {
 
 								var chart = new CanvasJS.Chart("chartContainer", {
@@ -21,12 +21,12 @@
 									},
 									data: [
 									{
-										type: "rangeColumn",
+										type: "error",
 										name: "Hours of Sleep",
 										showInLegend: true,
 										dataPoints: [
 											<?php foreach ($tStart as $key => $value): ?>
-											{ x: new Date(<?php echo $dos[$key][0].",".($dos[$key][1]-1).",".$dos[$key][2].",".$tStart[$key][0].",".$tStart[$key][1] ?>), y: [<?php echo $tStart[$key][0] ?>, <?php echo $tEnd[$key][0] ?>] },
+											{ x: new Date(<?php echo $dos[$key][0].",".($dos[$key][1]-1).",".$dos[$key][2].",".$tStart[$key][0].",".$tStart[$key][1] ?>), y: [<?php echo $tStart[$key][0] ?>, <?php echo $tEnd[$key][0] ?>], indexLabel:"<?php echo ((24-$tStart[$key][0])+ $tEnd[$key][0]); ?> hr of Sleep" },
 											<?php endforeach ?>
 										]
 									}, 
@@ -48,7 +48,7 @@
 										showInLegend: true,
 										dataPoints: [
 											<?php foreach ($tEnd as $key => $value): ?>
-												{ x: new Date(<?php echo $dos[$key][0].",".($dos[$key][1]-1).",".$dos[$key][2].",".$tEnd[$key][0].",".$tEnd[$key][1] ?>), y: <?php echo $tEnd[$key][0] ?> },
+												{ x: new Date(<?php echo $dos[$key][0].",".($dos[$key][1]-1).",".($dos[$key][2]+1).",".$tEnd[$key][0].",".$tEnd[$key][1] ?>), y: <?php echo $tEnd[$key][0] ?> },
 											<?php endforeach ?>
 										]
 									}]
--- application/views/Recommendations/sleeptracker_view.php
+++ application/views/Recommendations/sleeptracker_view.php
@@ -9,7 +9,7 @@
 				<div class="col-lg-12">
 					<h1>
 						<span class="glyphicon glyphicon-bed"></span>
-						Sleep Tracker
+						Sleep Tracker &nbsp <br><small>Suggested to be record every morning</small>
 					</h1>
 				</div>
 			</div>
@@ -20,12 +20,12 @@
 							<div class="row">
 								<form method="POST" action="<?php echo base_url('index.php/sleeptracker/saveSleepTracker');?>">
 									<div class="col-md-12">
-										<label for="dos" class="text-muted" id="white">Date Of Sleep</label>
+										<label for="dos" class="text-muted" id="white">Date Of Sleep </label>
 										<input id="dos" class="form-control" type="date" name="dateOfSleep" value="<?php echo date('Y-m-d'); ?>"></input><br>
 										<label for="st" class="text-muted" id="white">Sleep Time</label>
-										<input id="st" class="form-control" name="sleeptime" type="time" placeholder="Sleep Time..." value="00:00"></input><br>
-										<label for="wt" class="text-muted" id="white">Wake Up Time</label>
-										<input id="wt" class="form-control" name="wakeuptime" type="time" placeholder="Wake Up Time..." value="00:00"></input><br>
+										<input id="st" class="form-control" name="sleeptime" type="time" placeholder="Sleep Time..." value="20:00"></input><br>
+										<label for="wt" class="text-muted" id="white">Wake Up Time (Wake Up Time in the next day)</label>
+										<input id="wt" class="form-control" name="wakeuptime" type="time" placeholder="Wake Up Time..." value="06:00"></input><br>
 										<input type="submit" name="" value="Save" class="form-control btn btn-success">
 									</div>
 								</form>
