4c4816c078d5220b3acbfc74e30b525506027518
keirjeffreyvelez <keirjeffreyvelez18@gmail.com>
2018-03-09 03:06:28 +0800

    add additional data in watertracker

--- application/controllers/watertracker.php
+++ application/controllers/watertracker.php
@@ -20,18 +20,36 @@ class Watertracker extends CI_Controller {
 	function __construct(){
        	parent::__construct();
        	$this->load->model('waterintaketab');
+       	$this->load->model('assessments_tab');
+       	$this->load->model('results_tab');
        	$this->load->helper(array('form', 'url'));
    	}
 
 	public function index()
 	{	
 		$data['title'] = "Water Tracker | Liver-O";
+		$r = $this->results_tab->getResult();
+		$a = $this->assessments_tab->getTaken($this->session->userdata('userid'));
 		$data['waterintakeData'] = $this->waterintaketab->getwaterintake('asc');
 		if (!$data['waterintakeData']) {
 			$date['dateStartOfRecom'] = date('Y-m-d');
 			$this->session->set_userdata($date);			
 		}
-		$this->interpretWaterIntake();
+
+		if ($r) {
+			$data['sf36'] = json_decode($r[0]['qresults'], TRUE);
+			if ($data['sf36']['ave']>=75) {
+				$data['sf36_recom'] = $this->sf36_recom($data['sf36']);
+			}else{
+				$data['cldq'] = json_decode($r[2]['qresults'], TRUE);
+				$data['sf36_recom'] = $this->sf36_recom($data['sf36']);
+				$data['cldq_recom']=$this->cldq_recom($data['cldq']);
+			}
+
+		}
+
+		$data['waterAve'] = $this->interpretWaterIntake();
+		print_r($data['waterAve']);
 		$this->load->view('Recommendations/watertracker_view', $data);
 	}
 
@@ -41,6 +59,7 @@ class Watertracker extends CI_Controller {
 		$data['numberOfWaterIntake'] = $this->input->post('numOfIntake');
 		$data['userid'] = $this->session->userdata('userid');
 		$result = $this->waterintaketab->insertWaterIntake($data);
+
 		//$this->getNotification();
 		if ($result) {
 			redirect('watertracker', 'refresh');
@@ -54,10 +73,35 @@ class Watertracker extends CI_Controller {
 			$w+=$waterintakeData[$key]['numberOfWaterIntake'];
 		}
 		$a = $w/(count($waterintakeData));
-		print_r(round($a, 2));
+		return $a;
 	}
 
-	
+	function sf36_recom($score_mean=""){
+		if ($score_mean['ave']<75) {
+			$recom[1]=("Average Health is Unhealthy");
+			$recom[2]="";
+		} else {
+			if ($this->session->userdata('gender')=='Male') {
+				$recom[1]=("15 - 16 glasseses [125 ounces] per day");
+				$recom[2]=("7-9 hours of sleep per day");
+			}else{
+				$recom[1]=("11 - 12 glasses[92 ounces] per day");
+				$recom[2]=("7-9 hours of sleep per day");
+			}
+		}
+		return $recom;
+	}
+
+	function cldq_recom($score_mean=""){
+		if ($score_mean['ave']<50) {
+			$recom[1]=("6 - 7 glasses [51 ounces] per day");
+			$recom[2]="7-9 hours of sleep per day";
+		} else {
+			$recom[1]=("1 liter or 4 glasses [51 ounces] per day");
+			$recom[2]="≥ 8 hours of sleep per day";
+		}
+		return $recom;
+	}
 
 }
 
