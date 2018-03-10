f2f672432e80364237b9f00e7e90eba2d387d1e8
Franz08 <blicanel@yahoo.com>
2018-03-11 00:53:58 +0800

    Updated Recommendation Page, recommendation_view.php

--- application/views/Recommendations/recommendation_view.php
+++ application/views/Recommendations/recommendation_view.php
@@ -45,74 +45,77 @@
 				<div class=" tab-content">
 					<!-- ==========================================================WATER INTAKE RECOM====================================================================== -->
 					<div id="WaterTrackerMenu" class="col-lg-12  tab-pane fade in active">
-						<h3>
+						<!-- <h3>
 							<span class="glyphicon glyphicon-tint"></span>
 							Water Recommendation
-						</h3>
-						<p>Hey, you should know that your abdominial symptom severity score is: <?php echo $cldq['as']; ?> </p>
+						</h3> -->
 						<!-- ^======Insert recommendation here: this must change according to the score ======^ -->
-
-						<center><h3><?php echo $cldq_eval['as'] ?></h3></center>
-						<!-- ====================THE CHART OF CORRESPONDING RECOMMENDATION======================== -->
-
-						<p><strong>DISCLAIMER:</strong> The recommendations we provide cannot replace a real doctor's advice and prescription.</p>
-						<!-- ^======Insert disclaimer here: this must change according to the recommendation ======^ -->
+						<div>
+							<h4 class = "speech-bubble2 animated jackInTheBox">
+								Hey, you should know that your abdominial symptom severity score is: <strong><?php echo $cldq['as']; ?></strong>, 
+								<br> <?php echo $cldq_eval['as'] ?>
+							</h4>
+							<div class="pull-right">
+								<img class="center-block animated swing pull-right" style=" max-height: 12%; max-width: 12%; margin-right: 10%;" src="<?php echo base_url('Lib/imgs/Liver_D.png')?>" alt="liver Doctor"/>
+								<p class="pull-left"><strong>DISCLAIMER:</strong> The recommendations we provide cannot replace a real doctor's advice and prescription.</p>
+							</div>
+						</div>
 
 						<ul class="nav nav-tabs custom-nav-tabs">
-						    <li class="active"><a data-toggle="tab" href="#Wmenu1">Concerns</a></li>
-						    <li><a data-toggle="tab" href="#Wmenu2">Hints and Suggestion</a></li>
-						    <li><a data-toggle="tab" href="#Wmenu3">Additional Information</a></li>
+							<li class="active"><a data-toggle="tab" href="#Wmenu1">Concerns</a></li>
+							<li><a data-toggle="tab" href="#Wmenu2">Hints and Suggestion</a></li>
+							<li><a data-toggle="tab" href="#Wmenu3">Additional Information</a></li>
 						</ul>
 
 						<div class="tab-content custom-tab-content">
-						    <div id="Wmenu1" class="tab-pane fade in active">
-						      <h4>Concerns</h4>
-						      <ul>
-							      <?php if ($sf36['ave']>75): ?>
-							      	<li>
-							      		<h4>You are healthy, please keep it that way and regulate your water intake.</h4>
+							<div id="Wmenu1" class="tab-pane fade in active">
+							  	<h4>Concerns</h4>
+							  	<ul>
+								  	<?php if ($sf36['ave']>75): ?>
+								      	<li>
+								      		<h4>You are healthy, please keep it that way and regulate your water intake.</h4>
 
-							      	</li>
-							      <?php else: ?>
-							      	<?php if ($cldq['as']<50): ?>
-							      		<li>
-							      			<h4>You should keep watch over your water intake because your body might accumulate fluid that may worsen your abdominal symptoms</h4>
-							      		</li>
-							      		<li>
-							      			<h4>1 liter or 4 glasses [51 ounces] per day</h4>
-							      		</li>
-							      	<?php else: ?>
-							      		<li>
-							      			<h4>You are at risk of the effects of severe ascites, please consult your doctor and get fluid restriction instructions from your doctor.
-							      			</h4>
-							      		</li>
-							      		<li>
-							      			<?php echo $cldq_recom[2]; ?>
-							      		</li>
-							      	<?php endif ?>
-							      <?php endif ?>
-							     
-						     </ul>
-						    </div>
-						    <div id="Wmenu2" class="tab-pane fade">
-						      <h3>Hints and Suggestions</h3>
-						      <ul>
-						      	<li>According to most research, an average person needs 8 cups of 8 ounces of water as a minimum water intake for a day.</li>
-						      	<li>Drinking water before eating improves digestion.</li>
-						      	<li>After drinking or eating sugary treats, drinking water dilutes the sugar.</li>
-						      	<li>Always keep a water jug nearby to stay hydrated.</li>
-						      	<li>Eat ice chips & candy and gum to avoid thirst if ascites is severe.</li>
-						      </ul>
-						    </div>
-						    <div id="Wmenu3" class="tab-pane fade">
-						      <h3>Did you know?</h3>
-						      <ul>
-						      	<li>According to most research, An average human body is composed of 60% - 70%.</li>
-						      	<li>Some researches say that, drinking too much water too fast causes water intoxication or Hyponatermia.</li>
-						      	<li>Drinking water helps flush out toxins from the human body.</li>
-						      	<li>Adequate water intake can fix metabolism.</li>
-						      </ul>
-						    </div>
+								      	</li>
+								      <?php else: ?>
+								      	<?php if ($cldq['as']<50): ?>
+								      		<li>
+								      			<h4>You should keep watch over your water intake because your body might accumulate fluid that may worsen your abdominal symptoms</h4>
+								      		</li>
+								      		<li>
+								      			<h4>1 liter or 4 glasses [51 ounces] per day</h4>
+								      		</li>
+								      	<?php else: ?>
+								      		<li>
+								      			<h4>You are at risk of the effects of severe ascites, please consult your doctor and get fluid restriction instructions from your doctor.
+								      			</h4>
+								      		</li>
+								      		<li>
+								      			<?php echo $cldq_recom[2]; ?>
+								      		</li>
+								      	<?php endif ?>
+								    <?php endif ?>
+								     
+							    </ul>
+							</div>
+							<div id="Wmenu2" class="tab-pane fade">
+							    <h3>Hints and Suggestions</h3>
+							    <ul>
+							     	<li>According to most research, an average person needs 8 cups of 8 ounces of water as a minimum water intake for a day.</li>
+							     	<li>Drinking water before eating improves digestion.</li>
+							     	<li>After drinking or eating sugary treats, drinking water dilutes the sugar.</li>
+							     	<li>Always keep a water jug nearby to stay hydrated.</li>
+							     	<li>Eat ice chips & candy and gum to avoid thirst if ascites is severe.</li>
+							    </ul>
+							</div>
+							<div id="Wmenu3" class="tab-pane fade">
+							    <h3>Did you know?</h3>
+							    <ul>
+							      	<li>According to most research, An average human body is composed of 60% - 70%.</li>
+							      	<li>Some researches say that, drinking too much water too fast causes water intoxication or Hyponatermia.</li>
+							      	<li>Drinking water helps flush out toxins from the human body.</li>
+							      	<li>Adequate water intake can fix metabolism.</li>
+							    </ul>
+							</div>
 						</div>
 						<!-- ^======FOR MORE PRECISED RECOMMEDNATIONS ADD HERE FOR ADDITIONAL HINTS AND RECOMMENDATIONS ======^ -->
 					</div>
@@ -120,20 +123,22 @@
 
 					<!-- ================================================================SLEEP RECOM======================================================================= -->
 					<div id="SleepTrackerMenu" class="col-lg-12 tab-pane fade in">
-						<h3>
+						<!-- <h3>
 							<span class="glyphicon glyphicon-bed"></span>
 							Sleep Recommendation
-						</h3>
+						</h3> -->
 
-						<p>Hey tou should be aware of these following domains: <?php echo $sf36['ef']; ?></p>
+						<div>
+							<h4 class = "speech-bubble2 animated jackInTheBox">
+								Hey, you should know that your emotional function score is: <strong><?php echo $sf36['ef']; ?></strong>, 
+								<br> <?php echo $sf36_eval['ef'] ?>
+							</h4>
+							<div class="pull-right">
+								<img class="center-block animated swing pull-right" style=" max-height: 12%; max-width: 12%; margin-right: 10%;" src="<?php echo base_url('Lib/imgs/Liver_D.png')?>" alt="liver Doctor"/>
+								<p class="pull-left"><strong>DISCLAIMER:</strong> The recommendations we provide cannot replace a real doctor's advice and prescription.</p>
+							</div>
+						</div>
 
-						<!-- ^======Insert recommendation here: this must change according to the score ======^ -->
-
-						<center><h3><?php echo $sf36_eval['ef']; ?></h3></center>
-						<!-- ====================THE CHART OF CORRESPONDING RECOMMENDATION======================== -->
-
-						<p><strong>DISCLAIMER:</strong> The recommendations we provide cannot replace a real doctor's advice and prescription.</p>
-						<!-- ^======Insert disclaimer here: this must change according to the recommendation ======^ -->
 
 						<ul class="nav nav-tabs custom-nav-tabs">
 						    <li class="active"><a data-toggle="tab" href="#Smenu1">Concerns</a></li>
@@ -143,18 +148,25 @@
 
 						<div class="tab-content">
 						    <div id="Smenu1" class="tab-pane fade in active">
-						      <h4>Concerns</h4>
+						      <h4>Concerns: </h4>
 						      <!-- <p>If the result is high severity Systemic/Emotional/Fatigue place it here.</p> -->
+						      <ul>
 						      <?php if ($sf36['ef']>75): ?>
-						      	<p> You are healthy, please keep it that way and regulate your sleeping pattern to avoid complications.</p>
+						      	<li>
+						      		<h4> You are healthy, please keep it that way and regulate your sleeping pattern to avoid complications.</h4>
+						      	</li>
 						      <?php else: ?>
-						      	<?php if ($cldq['f']<50): ?>
-						      		<p>You should keep watch over your sleeping pattern because your body might suffer more serious complications involving sleep related liver complications. Keep track of your sleeping pattern before it's to late. </p>
-						      	<?php else: ?>
-						      		<p>You are at risk of the effects of severe hepatic encephalopathy, please consult your doctor and monitor your sleeping patterns. </p>
-						      	<?php endif ?>
+						      <?php if ($cldq['f']<50): ?>
+						      	<li>
+						      		<h4> You should keep watch over your sleeping pattern because your body might suffer more serious complications involving sleep related liver complications. Keep track of your sleeping pattern before it's to late. </h4>
+						      	</li>
+						      <?php else: ?>
+							    <li>
+							      	<h4> You are at risk of the effects of severe hepatic encephalopathy, please consult your doctor and monitor your sleeping patterns. </h4>
+							    </li>
 						      <?php endif ?>
-						      
+						      <?php endif ?>
+						      </ul>
 						    </div>
 						    <div id="Smenu2" class="tab-pane fade">
 						      <h3>Hints and Suggestions</h3>
@@ -183,13 +195,22 @@
 
 					<!-- ================================================================CALORIE RECOM===================================================================== -->
 					<div id="CalorieRecomMenu" class="col-lg-12 tab-pane fade in">
-						<h3>
+						<!-- <h3>
 							<span class="glyphicon glyphicon-cutlery"></span>
 							Calories/Carbohydrates/Fats Recommendations
-						</h3>
-						<p>Show score or reason why the recommended amount is generated</p>
-						<p style="font-weight: bold; font-size: 15px">Your Body Mass Index: <span style="font-weight: bold"><?php echo $bmi; ?></span></p>
-						<!-- ^======Insert recommendation here: this must change according to the score ======^ -->
+						</h3> -->
+
+						<div>
+							<h4 class = "speech-bubble2 animated jackInTheBox">
+								According to your assessment your Body Mass Index is <strong><?php echo $bmi; ?></strong>,
+								<br>
+								Your daily recommended Calorie intake should be, <strong><?php echo ($bmi*40); ?> kcal/kg</strong> per day.
+							</h4>
+							<div class="pull-right">
+								<img class="center-block animated swing pull-right" style=" max-height: 12%; max-width: 12%; margin-right: 10%;" src="<?php echo base_url('Lib/imgs/Liver_D.png')?>" alt="liver Doctor"/>
+								<p class="pull-left"><strong>DISCLAIMER:</strong> Please go to your dietician for interpretation of the results and get an appropriate meal plan</p>
+							</div>
+						</div>
 
 						<!-- <center>
 							<h4>
@@ -205,7 +226,7 @@
 						</center> -->
 						<!-- ====================THE CHART OF CORRESPONDING RECOMMENDATION======================== -->
 
-						<p><strong>DISCLAIMER:</strong> The recommendations we provide cannot replace a real doctor's advice and prescription.</p>
+
 						<!-- ^======Insert disclaimer here: this must change according to the recommendation ======^ -->
 
 						<ul class="nav nav-tabs custom-nav-tabs">
@@ -219,9 +240,10 @@
 						      <h4>Concerns</h4>
 
 						      <ul>
-							      <li>Recommended Calorie Intake: <?php echo ($bmi*40); ?> kcal/kg</li>
-							      <li>Recommended Cardohydrates Intake: <?php echo (($bmi*40)*0.5)/4; ?> g</li>
-							      <li>Recommended Fats Intake: <?php echo round(((($bmi*40)*0.35)/9),2); ?> g</li>
+							      <li>Recommended Calorie Intake: <strong><?php echo ($bmi*40); ?> kcal/kg</li></strong>
+							      <li>Recommended Cardohydrates Intake: <strong><?php echo (($bmi*40)*0.5)/4; ?> g</li></strong>
+							      <li>Recommended Fats Intake: <strong><?php echo round(((($bmi*40)*0.35)/9),2); ?> g</li></strong>
+							      <li>Recommended Protein Intake: <strong><?php echo round(((($bmi*40)*0.15)/4),2); ?> g</li></strong>
 						      </ul>
 						      <!-- <?php if (condition): ?>
 						      	
